aa5cd5d3370e4986ca2d4e61a7cf76edd723560d
Marmot McMahon <marmotmcmahon@gmail.com>
2018-09-24 23:15:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,5 @@
 var katzDeliLine = [];
 
-function takeANumber(katzDeliLine, name) {
-  katzDeliLine.push(name)
-  return "Welcome, " + name + ". You are number " + katzDeliLine.length + " in line."
-}
-
 function takeANumber(katzDeliLine, person) {
   katzDeliLine.push(person);
   return "Welcome, " + person + ". You are number " + katzDeliLine.length + " in line.";
