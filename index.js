e2abe9ead19978ffffcf76f8b75155e5504fd78a
Marmot McMahon <marmotmcmahon@gmail.com>
2018-09-24 23:26:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,5 +6,9 @@ function takeANumber(katzDeliLine, person) {
 }
 
 function nowServing(katzDeliLine) {
-  
+  if (katzDeliLine.length == 0) {
+    return "There is nobody waiting to be served!";
+  } else {
+    return katzDeliLine[0];
+  }
 }
